ca764b740e4106dac048c18ebe668c2de3c6bf07
mingyu6688 <kmingyu6688@gmail.com>
2024-11-06 20:35:52 +0900

    Reapply "Merge pull request #2 from mingyu6688/develop"
    This reverts commit 7e7d06a0cc1aef30833d34b6e16a9b1508b6777b.

---
+++ src/db/database.js
@@ -0,0 +1,27 @@
+import mysql from 'mysql2/promise';
+import { config } from '../config/config.js';
+import { formatDate } from '../utils/formatDate.js'
+
+const createPool = () => {
+    const pool = mysql.createPool({
+        ...config.database,
+        waitForConnections: true,
+        connectionLimit: 10,
+        queueLimit: 0,
+    });
+
+    const originalQuery = pool.query;
+
+    pool.query = (sql, params) => {
+    const date = new Date();
+
+    console.log(`[${formatDate(date)}] Excuting query: ${sql} ${params ? `, ${JSON.stringify(params)}` : ``}`);
+
+    return originalQuery.call(pool, sql, params);
+    };
+    return pool;
+};
+
+const dbPool = createPool();
+
+export default dbPool;
