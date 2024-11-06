522d82e5bc06c785d70ccb90da6969360b1f0cbf
sdh02142 <sdh02142@naver.com>
2024-11-06 17:58:49 +0900

    Update : 회원가입, 로그인 기능(user.db.js) 추가 및 클라이언트 작동 확인 - #4
    Co-authored-by: DudeKYH <DudeKYH@users.noreply.github.com>

--- src/handlers/login.handler.js
+++ src/handlers/login.handler.js
@@ -1,15 +1,30 @@
 import { PACKET_TYPES } from '../constants/packetTypes.js';
 import { createResponse } from '../utils/response/createResponse.js';
+import { SECRETKEY } from '../constants/env.js';
+import JWT from 'jsonwebtoken';
 
 const loginHandler = async ({ socket, userId, payload }) => {
-  const { id, password, email } = payload;
+  const { id, password } = payload;
 
-  console.log(id, password, email);
+  console.log(id, password);
+
+  const token = JWT.sign({
+    id: id,
+    password: password
+  }, SECRETKEY, { expiresIn: "1h" });
+
+  console.log('-----------------------');
+  console.log( token );
+  
+  const verified = JWT.verify(token, SECRETKEY)
+
+  console.log('-----------------------');
+  console.log( verified );
 
   const loginResponse = createResponse(PACKET_TYPES.LOGIN_RESPONSE, {
-    success: 0,
-    message: 'Success',
-    token: 0,
+    success: true,
+    message: 'success',
+    token: token,
     failCode: 0,
   });
 
