b6fb596203f3c16792920e91440c587c6da99a18
liha-engineer <liha.engineer@gmail.com>
2024-11-08 14:59:36 +0900

    Fix : 타워 클래스 메서드를 유저 클래스로 이동 - #8

---
+++ src/classes/models/tower.class.js
@@ -0,0 +1,44 @@
+class Tower {
+  constructor(x, y, userId, towerId) {
+    this.x = x;
+    this.y = y;
+    this.userId = userId;
+    this.towerId = towerId;// 고유한 무언가... 
+    this.beamDuration = 0;
+    this.tartget = null;
+  }
+
+  draw(ctx) {
+    const towerImage = new Image();
+    towerImage.src = this.image;
+    ctx.drawImage(towerImage, this.x, this.y, this.width, this.height);
+
+    if (this.beamDuration > 0 && this.target) {
+      ctx.beginPath();
+      ctx.moveTo(this.x + this.width / 2, this.y + this.height / 2);
+      ctx.lineTo(this.target.x + this.target.width / 2, this.target.y + this.target.height / 2);
+      ctx.strokeStyle = '#FFFF99';
+      ctx.lineWidth = 10;
+      ctx.stroke();
+      ctx.closePath();
+      this.beamDuration--;
+    }
+  }
+
+  attack(monster) {
+    if (this.cooldown <= 0) {
+      monster.hp -= this.attackPower; // 몬스터의 HP 감소
+      this.cooldown = 180; // 3초  60 당 1 초
+      this.beamDuration = 30; // 광선 지속 시간
+      this.target = monster; // 광선의 목표 설정
+    }
+  }
+
+  updateCooldown() {
+    if (this.cooldown > 0) {
+      this.cooldown--;
+    }
+  }
+}
+
+export default Tower;
--- src/classes/models/user.class.js
+++ src/classes/models/user.class.js
@@ -1,3 +1,5 @@
+import Tower from "./tower.class.js";
+
 class User {
   constructor(id, socket) {
     this.id = id;
@@ -6,11 +8,32 @@ class User {
     this.sequence = 0;
     this.score = 0;
     this.gold = 0;
+    this.towers = [];
   }
 
   getNextSequence() {
     return ++this.sequence;
   }
+
+  initializeTower = () => {
+    this.towers = [];
+  };
+
+    getAllTowers = () => {
+    return this.towers;
+  };
+
+    towerNumber = () => {
+    return this.towers.length;
+  };
+  
+    bindTower = (payload, userId) => {
+     const { x, y } = payload;
+     const coordinateX = x;
+     const coordinateY = y;
+     const tower = new Tower(coordinateX, coordinateY, userId)
+    return this.towers.push(tower);
+  };
 }
 
 export default User;
