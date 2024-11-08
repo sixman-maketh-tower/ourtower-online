165eb1928a706db3598d1d6bd5d6e9947bf1d325
liha-engineer <liha.engineer@gmail.com>
2024-11-08 14:56:00 +0900

    Add : 타워 핸들러 생성 - #8

---
+++ src/handlers/tower.handler.js
@@ -0,0 +1,37 @@
+import Tower from "../classes/tower.class.js";
+import { PACKET_TYPES } from "../constants/packetTypes.js";
+import { getServerGameAssets } from "../init/assets.js";
+import { getGameSessions } from "../session/game.session.js";
+import { createResponse } from "../utils/response/createResponse.js";
+
+let towerId = 0;
+
+const towerPurchaseHandler = async ({ socket, userId, payload }) => {
+    // 유저가 접속한 게임세션 찾기
+    // 게임에 속한 유저 데이터 중 towers 배열에 구매요청한 타워 추가
+    // 타워구매 응답패킷 만들어 클라이언트에 반환
+    
+    const { x, y } = payload;
+    const { towerData } = getServerGameAssets();
+
+    const gameSession = getGameSessions();
+    if (!gameSession) {
+        console.error('게임 세션이 존재하지 않습니다.')
+    }
+    const user = gameSession.getUser(userId)
+    if (!user) {
+        console.error('해당 유저가 속한 게임세션이 존재하지 않습니다.')
+    }
+
+    user.bindTower(x, y, userId, towerId)
+
+    towerId += 1;
+    console.log('towerId?: ', towerId)
+    const towerPurchaseResponse = createResponse(PACKET_TYPES.TOWER_PURCHASE_RESPONSE, {
+        towerId: towerId
+    })
+
+    socket.write(towerPurchaseResponse);
+}
+
+export default towerPurchaseHandler;
