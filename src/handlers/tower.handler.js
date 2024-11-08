a80ee1e8c3e425eac9a8b0843cc622965529cf58
liha-engineer <liha.engineer@gmail.com>
2024-11-08 21:42:16 +0900

    Fix : 게임세션 찾는 방식 수정 및 enemyId를 user.id로 검출하도록 수정 - #8

--- src/handlers/tower.handler.js
+++ src/handlers/tower.handler.js
@@ -1,9 +1,9 @@
 import { PACKET_TYPES } from "../constants/packetTypes.js";
 import { getServerGameAssets } from "../init/assets.js";
 import { getGameSessions } from "../session/game.session.js";
+import { getUserBySocket } from "../session/user.session.js";
 import { createResponse } from "../utils/response/createResponse.js";
 
-let towerId = 0;
 
 const towerPurchaseHandler = async ({ socket, userId, payload }) => {
     // 유저가 접속한 게임세션 찾기
@@ -11,23 +11,26 @@ const towerPurchaseHandler = async ({ socket, userId, payload }) => {
     // 타워구매 응답패킷 만들어 클라이언트에 반환
     
     const { x, y } = payload;
-    const { towerData } = getServerGameAssets();
+    const { towers } = getServerGameAssets();
 
-    const gameSession = getGameSessions();
-    if (!gameSession) {
-        console.error('게임 세션이 존재하지 않습니다.')
-    }
-    const user = gameSession.getUser(userId)
+    // 게임세션 안에 있는 유저를 찾는 것
+    const user = getUserBySocket(socket)
     if (!user) {
-        console.error('해당 유저가 속한 게임세션이 존재하지 않습니다.')
+        console.error('유저가 존재하지 않습니다.')
     }
+    const towerCost = towers.data[0].cost
+    user.gold -= towerCost; 
+    
+    const gameSession = getGameSessions().find((game) => game.users.includes(user));
+    
+    const enemyId = gameSession.getOpponentUserId(user.id)
+    const enemy = gameSession.getUser(enemyId)
 
-    user.bindTower(x, y, userId, towerId)
-
-    towerId += 1;
-    console.log('towerId?: ', towerId)
+    // 이때 잡아오는건 적의 소켓을 잡아와서 bindTower에 넣어줘야 적 소켓에다 노티를 쏠 수 있다
+    const newTower = user.bindTower(enemy.socket, x, y);
+    
     const towerPurchaseResponse = createResponse(PACKET_TYPES.TOWER_PURCHASE_RESPONSE, {
-        towerId: towerId
+        towerId: newTower.id
     })
 
     socket.write(towerPurchaseResponse);
