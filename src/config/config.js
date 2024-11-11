b76ed5b0cfab2586b37e1f925b11a215f05bcb0a
Dam2204 <soyoung4576@gmail.com>
2024-11-11 14:35:56 +0900

    Fix: config INIT_BASE_DATA 수정, game.proto>> monsterType 추가

--- src/config/config.js
+++ src/config/config.js
@@ -19,8 +19,7 @@ import {
   CANVAS_WIDTH,
   GAME_STATE_PLAYING,
   GAME_STATE_WAITING,
-  INIT_BASE_HP,
-  INIT_BASE_MAX_HP,
+  INIT_BASE_DATA,
   INIT_GOLD,
   INIT_MONSTER_SPAWN_INTERVAL,
   INIT_TOWER_COST,
@@ -48,7 +47,7 @@ export const config = {
       playing: GAME_STATE_PLAYING,
     },
     initData: {
-      baseHp: INIT_BASE_HP,
+      baseHp: INIT_BASE_DATA.maxHp,
       towerCost: INIT_TOWER_COST,
       gold: INIT_GOLD,
       monsterSpawnInterval: INIT_MONSTER_SPAWN_INTERVAL,
@@ -57,10 +56,7 @@ export const config = {
       width: CANVAS_WIDTH,
       height: CANVAS_HEIGH,
     },
-    baseData: {
-      hp: INIT_BASE_HP,
-      maxHp: INIT_BASE_MAX_HP,
-    },
+    baseData: INIT_BASE_DATA,
   },
   database: {
     database: DB_NAME,
--- src/constants/game.js
+++ src/constants/game.js
@@ -6,8 +6,10 @@ export const GAME_STATE_PLAYING = 'playing';
 export const CANVAS_WIDTH = 1300;
 export const CANVAS_HEIGH = 360;
 
-export const INIT_BASE_HP = 300;
-export const INIT_BASE_MAX_HP = 300;
+export const INIT_BASE_DATA = {
+  hp: 300,
+  maxHp: 300,
+};
 
 export const INIT_TOWER_COST = 100;
 export const INIT_GOLD = 100000;
